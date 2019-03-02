a27bcacacefa42c3cfa449c4718e5d25b2f26f8d
LSR <LSR1002262798@outlook.com>
2019-03-02 16:32:58 +0800

    hw0

--- ta/hw0/main.c
+++ ta/hw0/main.c
@@ -2,9 +2,20 @@
 #include <sys/resource.h>
 
 int main() {
-    struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    struct rlimit lim; 
+    if(getrlimit(RLIMIT_STACK, &lim) < 0){  
+        perror("getrlimit");  
+    }      
+    printf("stack size: %ld\n", lim.rlim_cur);
+
+    if(getrlimit(RLIMIT_NPROC, &lim) < 0){
+        perror("getrlimit");        
+    }   
+    printf("process limit: %ld\n", lim.rlim_cur);
+
+    if(getrlimit(RLIMIT_NOFILE, &lim) < 0){
+        perror("getrlimit");        
+    }   
+    printf("max file descriptors: %ld\n", lim.rlim_cur);
     return 0;
 }
--- ta/hw0/wc.c
+++ ta/hw0/wc.c
@@ -1,3 +1,56 @@
-int main(int argc, char *argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void count(char *name, FILE *file){
+    int line_count = 0;
+    int word_count = 0;
+    int byte_count = 0;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int in_word = 0;
+ 
+    while((read = getline(&line, &len, file)) != -1){
+        size_t size = strlen(line);
+        for(int i = 0; i < size; i++){
+            byte_count += 1;
+            char ch = line[i];
+            if(ch == '\n'){
+                line_count += 1;
+            }
+
+            if(ch == ' ' || ch == '\n'){
+                in_word = 0;
+            }else if(!in_word){
+                word_count += 1;
+                in_word = 1;
+            }
+        }       
+    }
+    if(name){
+        printf("%d  %d  %d  %s\n", line_count, word_count, byte_count, name);
+    }else{
+        printf("%d  %d  %d\n", line_count, word_count, byte_count);
+    }
+} 
+
+int main(int argc, char *argv[]){
+    char* name = NULL;
+    FILE* file = NULL;
+ 
+    if(argc == 1){
+        file = stdin;
+        count(name, file);
+    }else if(argc == 2){
+        name = argv[1];
+        file = fopen(name, "r");
+        if(!file){
+             printf("cannot open this file!");
+             exit(1);
+        }
+        count(name, file);
+    }
     return 0;
 }
